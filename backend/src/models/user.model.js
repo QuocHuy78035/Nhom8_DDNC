c2f209c8ba8a282cea84f6bb2e0376f07da53c90
longbui2k3 <buiduclongit@gmail.com>
2024-03-04 16:01:22 +0700

    C

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["user", "admin"],
+      enum: ["user", "admin", "vendor"],
       default: "user",
     },
     avatar: { type: String, default: default_image },
